61bc62838b003cfe80feaf7f64091e42db608cc0
Cristian Gutierrez <cdgutierrez456@gmail.com>
2024-08-21 23:01:05 -0500

    Estructuracion inicial... Se procede a hacer los primeros cambios superficiales del componente header para que tenga una forma estetica para el usuario [header]Cambios aplicados en el componente header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,28 @@
+
+import personIcon from '../../public/img/personIcon.svg'
 import '../styles/Header.css'
 
 export default function Header() {
   return (
     <header className='header'>
       <nav>
-        <img 
-          src="" 
-          alt="" 
-        />
+        <section className='header__section'>
+          <img 
+            className='header__img' 
+            src={personIcon} 
+            alt="Icono de perfil"
+          />
+          <ul>
+            <li>Opcion 1</li>
+            <li>Opcion 2</li>
+          </ul>
+        </section>
+        <section className='header__section'>
+          <ul>
+            <li>Opcion 3</li>
+            <li>Opcion 4</li>
+          </ul>
+        </section>
       </nav>
     </header>
   )
